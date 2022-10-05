e6052d1a6816d8183292ddf77891699d40bf6b1e
kath424 <acostak424@gmail.com>
2022-10-05 08:25:50 -0300

    Ejercicio listas-objetos-date

---
+++ cursoJavascript/listas-objetos-fechas/ejercicioLista.js
@@ -0,0 +1,13 @@
+const datosPersonales = [
+  'Katerine Acosta',
+  26,
+  true,
+  new Date('march 14 1996'),
+
+  libroFavorito = {
+    titulo: 'El arte de amar',
+    autor: 'Erich Fromm',
+    fecha: new Date('january 1 2014'),
+    url: 'https://www.amazon.com.mx/El-arte-amar-Erich-Fromm/dp/9688530891'
+  }
+]

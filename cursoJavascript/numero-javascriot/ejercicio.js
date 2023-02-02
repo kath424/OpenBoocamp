46ccc34cbe7f6d3a153a6a4986841c6f6fee9bbe
kath424 <acostak424@gmail.com>
2023-02-02 19:50:02 -0300

    Correccion de Ejercicio

--- cursoJavascript/numero-javascriot/ejercicio.js
+++ cursoJavascript/numero-javascriot/ejercicio.js
@@ -3,7 +3,7 @@ let altura = 160;
 console.log("Tu altura es: " + altura);
 
 //Una variable que contenga tu altura en metros (número de coma flotante)
-let alturaMetros = 1.61;
+let alturaMetros = 1.6;
 console.log("Tu altura en metros es: " + alturaMetros);
 
 //Una variable que contenga tu peso en kilogramos (número de coma flotante)
@@ -11,8 +11,11 @@ let peso = 55.7;
 console.log("Tu peso es: " + peso);
 
 //Una variable que contenga tu altura en metros redondeada hacia arriba
-let alturaMetroArriba = alturaMetros;
-console.log(Math.ceil(alturaMetros));
+let alturaMetroArriba = Math.ceil(alturaMetros);
+console.log(alturaMetroArriba);
 //Una variable que contenga tu peso en kilogramos redondeado hacia abajo
-
+let pesoAbajo = Math.floor(peso);
+console.log(pesoAbajo);
 //Una variable que contenga si "el máximo valor que se puede obtener en Javascript + 1" es igual al máximo valor que se puede obtener en Javascript
+let sonIguales = Number.MAX_VALUE + 1 === Number.MAX_VALUE;
+console.log(sonIguales);
